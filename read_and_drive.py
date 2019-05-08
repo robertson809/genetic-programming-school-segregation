14f45e9fab6d4104163570b91ea9ccce9f810f62
Michael Robertson <mirobertson@davidson.edu>
2019-05-08 02:41:16 -0400

    Can create children

--- read_and_drive.py
+++ read_and_drive.py
@@ -1,8 +1,11 @@
+from __future__ import division
 import csv
 import random
 import math
 import time
-k = 25000
+import copy
+
+k = 250 
 over_capacity_assignment_count = 0
 #####################################################################################
 #               ReadData                                                            #
@@ -17,19 +20,17 @@ over_capacity_assignment_count = 0
 #####################################################################################
 def readData(roads, schools):
     with open(roads) as rc_file, open(schools) as schools_file:
-        #dialect = csv.Sniffer().sniff(csv_file.read(1024))
-        #csv_file.seek(0)
         #Each row read from the csv file is returned as a list of strings
         rc_dataset = list(csv.reader(rc_file,delimiter=','))
         schools_dataset = list(csv.reader(schools_file, delimiter=','))
         
         road_list = []
-        count = 0
+        row_count = 0
         for row in rc_dataset:
             #testing stopping condition for small datasets
-            # count += 1
-#             if count == k:
-#                 break
+            row_count += 1
+            # if row_count == k:
+            #     break
             road_id = int(row[0])
             ses = int(row[3])
             pop = float(row[1])
@@ -37,10 +38,11 @@ def readData(roads, schools):
             #read in schools
             schools = row[8:]
             close_schools = []
-            for dist in schools:
+            for school_count in range(len(schools)):
                 #only read in real distances
-                if float(dist) < 99999:
-                    tup = (schools.index(dist), float(dist))  #schools.index(dist) is the number of the school
+                dist = float(schools[school_count])
+                if dist < 99999:
+                    tup = (school_count, dist)  #is the number of the school in the column
                     close_schools.append(tup)
                     
             #keep list of roads 
@@ -51,18 +53,8 @@ def readData(roads, schools):
         schools_dataset.pop(0) #take out the header
         for row in schools_dataset:
             school_list.append(School(float(row[4])))
-            
-            #testing stopping conditition for smaller dataset
-            # if schools_dataset.index(row) == 700:
-#                 break
-            
-        return District(road_list, school_list)
 
-class Switcher(object):
-    def ses_to_func(self, arg):
-        """Dispatch method"""
-        method_name = 'add_' + str(arg)  
-        method = getattr(School, method_name)      
+        return District(road_list, school_list)   
 class School:
     def __init__(self, cap):
         self.cap = cap
@@ -70,15 +62,37 @@ class School:
         self.mid_std = 0
         self.high_std = 0
         
-    def add_1(self, arg):
-        self.low_std += arg
+        self.low_pro = 0
+        self.mid_pro = 0
+        self.high_pro = 0
         
-    def add_2(self, arg):
-        self.mid_std += arg
+        self.weight = 0
         
-    def add_3(self, arg):
-        self.high_std += arg
+    def calcPro(self):
+        total = self.low_std + self.mid_std + self.high_std
+        if total == 0:
+            return
+        self.low_pro = self.low_std / total 
+        self.mid_pro = self.mid_std / total
+        self.high_pro = self.high_std / total
         
+        self.weight = abs((1.0/3.0) - self.low_pro) + abs((1.0/3.0) - self.mid_pro) + abs((1.0/3.0) - self.high_pro) + 1
+
+    
+    def pretty_print(self):
+        print 'this school has ', self.low_std, ' low ses students ', self.mid_std, ' mid ses students ', \
+        self.high_std, ' high ses students , so it is ', self.low_pro , ' percent low and ', self.low_pro + \
+        self.mid_pro + self.high_pro , ' = 1'
+    
+    def reset(self):
+        self.low_std = 0
+        self.mid_std = 0
+        self.high_std = 0
+        
+        self.low_pro = 0
+        self.mid_pro = 0
+        self.high_pro = 0
+
     ##################################################
     #Road class has three instances
     #SES - 1 LOW
@@ -124,123 +138,118 @@ class District:
     #   @returns true if assignment possible, and also makes the assignment, and updates the SES total #
     #   @returns false if assignment not possible
     #################################################################################
-    def check_capacity(self, current_assignment, road_id, school_id):
+    def check_capacity(self, current_assignment, position, road, school_id):
         cap = self.school_list[school_id].cap
-        pop = self.road_list[road_id].pop
+        pop = road.pop
         if self.running_cap[school_id] + pop > cap * 1.25:
             global over_capacity_assignment_count 
             over_capacity_assignment_count += 1
             return False
         else:
-            current_assignment[road_id] = school_id
+            current_assignment[position] = school_id
             self.running_cap[school_id] += pop
-            ses = self.road_list[road_id].ses
+            ses = road.ses
             
-            method_name = 'add_' + str(ses)  
-            add_method = getattr(self.school_list[0], method_name)
-            add_method(pop)   
+            #this is actually faster than switching on the methods
+            if ses == 1:
+                self.school_list[school_id].low_std =+ pop
+            elif ses == 2:
+                self.school_list[school_id].mid_std =+ pop
+            elif ses == 3:
+                self.school_list[school_id].high_std =+ pop
+            else: 
+                raise Exception('invaldid SES, the SES is ', ses)
             return True
+        
             
-    def ses_to_func(self, arg):
-        """Dispatch method"""
-        method_name = 'add_' + str(arg)  
-        method = getattr(self.school_list[0], method_name)
-    
-            
-     #############################################################################
-     #                     New Gen                                               #
-     # @param num_schools - the number of schools                                # 
-     # @param num_roads - the number of roads                                    #
-     # @param road_list - list of road objects                                   #
-     # @param school_list - list of schools capacities, "ID" is position and     #
-      ############################################################################  
+     #######################################################################################################
+     #                                              Spawn                                                  #
+     #                                                                                                     #
+     # @returns assign_obj - an assignment object with road_id as the index,                               #
+     #                        #school_id as the school, and none objects entered if no assignment possible #
+     # #####################################################################################################  
     def spawn(self):
-        #re-initialize the running capacity
-        self.running_cap = [0] * len(self.school_list)
+        best_fitness = float('inf')
+        self.running_cap = [0] * len(self.school_list)#re-initialize the running capacity and school populations
+        for school in self.school_list:
+            school.reset() 
+            
         assignment = [None] * len(self.road_list)
-        for road_num in range(len(self.road_list)):
-            start = time.time()
-            if road_num % 100 == 0:
-                print ' it took ', time.time() - start, ' seconds to  assign the last 100 schools,' \
-                'which takes us up to road number ', road_num
-                start = time.time()
+        count = 0
+        for road in self.road_list:
             assigned = False
+            tried = set(())
             while not assigned:
-                assigned = self.check_capacity(assignment, road_num, random.randint(0,len(self.school_list) - 1)) \
-                #last arguement is random school id
+                #there are no listed close schools
+                if len(road.close_schools) == 0:
+                    assigned = True
+                    break
+                five_close = road.close_schools #not necessarily five schools
+                random_close = five_close[random.randint(0, len(five_close) - 1)]
+                random_school_id = random_close[0] 
+                tried.add(random_school_id)
+                if len(tried) == len(five_close):
+                    break #we have tried every assignment and none work
+                random_dist = random_close[1] #comment out
+                assigned = self.check_capacity(assignment, count, road, random_school_id) #last arguement is school_id
+                
+            count += 1
         assign_obj = Assignment(assignment)
-        return assign_obj
+        
+        ######Calculate proportions######
+        #make sure we don't try to calculated the proprotion of a school with no one
+        for school_id in range(len(self.school_list)):
+            if self.running_cap[school_id] != 0.0:
+                self.school_list[school_id].calcPro()
+            
+        assign_obj.calcFitness(self.school_list, self)
+        if assign_obj.fitness < best_fitness:
+            if assign_obj.fitness < 10:
+                assign_obj.pretty_print()
+            best_fitness = assign_obj.fitness
+            king = copy.deepcopy(assign_obj)
+        return assign_obj, king
         
         
 class Assignment:
     def __init__(self, assignment):
         self.rs_list = assignment
-        self.fitness = self.calcFitness()
-    #use Lena's function here
-    def calcFitness(self):
-        return 10
-        #find SES of the school
-    def pretty_print(self):
-        print self.assignment, ', fitness: ', self.fitness
- 
+        self.fitness = float('inf')
         
-###############Driver methods############
-def new_gen(size, district, tol):
-    """
-    :param data: the district from which to create new children
-    :return tuple: the new generation in the first entry,
-    the very best individual in the second entry
-    """
-    forest = []
-    best_fitness = float('inf')
-    king = None
-    ##create population and rank them by fitness
-    for i in range(size):
-        cur = district.spawn()
-        forest.append(cur)
-        # calculate fitness
-        print("the fitness of new tree ", i, " is ", cur.fitness)
-        if cur.fitness < best_fitness:
-            best_fitness = cur.fitness
-            king = cur
-            if cur.fitness < tol:
-                print ' we broke tolerance, YAY! ', cur.pretty_print()
-                break
-    return forest, king
+    #use Lena's function here
+    def calcFitness(self, populated_school_list, district): #populated_school_list is the number of people (entry) in each school (id)
+        self.fitness = 0
+        for road_num in range(len(self.rs_list)): #find the distance from the road to the school it's assigned to
+            if self.rs_list[road_num] == None:
+                continue
+            five_schools = district.road_list[road_num].close_schools
+            for tup in five_schools:
+                if self.rs_list[road_num] == tup[0]: #tup[0] is school_id
+                    dist = tup[1]
+                    weight = district.school_list[tup[0]].weight
+                    self.fitness += dist * weight
+                    break
+        
+    def pretty_print(self):
+        print 'the child has fitness: ', self.fitness
 
-def calcFitness(self, data):
-    # squared error
-    sqrerr = 0
-    # use each set of data points
-    for row in range(len(data)):
-        # get a data point
-        list_i = data[row]
-
-        # take the input and leave the output
-        input_i = list_i[0:self.input_dim]
-        #print(input_i)
-        # convert from list to tuple
-        input_tup = tuple(input_i)
-        # evaluate based on the tuple
-        f_out = self.evaluate(input_tup)
-
-        sqrerr += (f_out - data[row][-1]) ** 2
-    mse = sqrerr / len(data)
-    rmse = mse ** (.5)
-    # Need to add size penalty, add 2.5% of the total fitness for each node
-    rmse += SIZE_PENALTY_COEFF * self.size * rmse
-    self.fitness = mse
-    return mse
  
 
-BG_district = readData("rc-bluegreen.csv", "schoolsbluegreenDescribed.csv")
-#BG_district.pretty_print()
+BG_district = readData("rc-greyviolet.csv", "schoolsgreyviolet.csv")
 n = 100
 start = time.time()
 start2 = time.clock()
-BG_district.spawn()
-new_gen(n, BG_district, 4)
+children = []
+best_fit = float('inf')
+king = None
+for i in range(n):
+    children.append(BG_district.spawn())
+    children[i][0].pretty_print()
+    if children[i][1].fitness < best_fit:
+        king = children[i][1]
+        best_fit = king.fitness
+        print 'the new king has fitness ', king.fitness
+print 'the best fitness we found was', king.fitness    
+
 print 'the total number of overcapacity assignments is ', over_capacity_assignment_count
-print 'It took ', time.time() - start, ' to create ', n, ' assignments with ', len(BG_district.road_list), ' roads'
-print 'By another measure'
-print 'It took ', time.clock() - start2, ' to create ', n, ' assignments with ', len(BG_district.road_list), ' roads'
+print 'The process took ', time.time() - start, ' to create ', n, ' assignments with ', len(BG_district.road_list), ' roads'
